d0ca31621ef9a88077f94557c9ec3d856d06bde8
Walid <walid.bensalah20@gmail.com>
2025-05-14 10:02:32 +0200

    add error 404 page content

--- src/pages/error-404/Error404.jsx
+++ src/pages/error-404/Error404.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './styles.scss';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
-  )
-}
+    <div className='error-container'>
+      <h1 className='error-title'>404</h1>
+      <h2 className='error-message'>
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <p>
+        <Link to='/'>Retourner sur la page d’accueil</Link>
+      </p>
+    </div>
+  );
+};
 
-export default Error404
+export default Error404;
